57ba892d8ef6b11adaf32cb45bd277cb527feacc
Susan Luvisi <susaneluvisi@gmail.com>
2023-03-08 14:20:37 +0000

    Completed first part

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def index
+    # code to display a list of all restaurants
+  end
+
+  def show
+    # code to display a single restaurant
+  end
+
+  def new
+    # code to display a form for creating a new restaurant
+  end
+
+  def create
+    # code to create a new restaurant in the database
+  end
+
+  def edit
+    # code to display a form for editing a restaurant
+  end
+
+  def update
+    # code to update an existing restaurant in the database
+  end
+
+  def destroy
+    # code to delete a restaurant from the database
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read all
+  get "restaurants", to: "restaurants#index"
+  # Create
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  # Read one
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+  # resources :restaurants
+
 end
